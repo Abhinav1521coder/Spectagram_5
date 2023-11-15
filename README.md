b4932dc8a732e9383f5032b2a51a469d92a17e08
Abhinav1521coder <118897608+Abhinav1521coder@users.noreply.github.com>
2023-11-15 21:10:31 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Spectagram_5
+# spectagram-stage-4
+project solution c84
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDBdGqLIyZfx8uS-m-9RHVvuLql0YlSCyA",
+    authDomain: "hot-air-balloon-cc82f.firebaseapp.com",
+    databaseURL: "https://hot-air-balloon-cc82f-default-rtdb.firebaseio.com",
+    projectId: "hot-air-balloon-cc82f",
+    storageBucket: "hot-air-balloon-cc82f.appspot.com",
+    messagingSenderId: "524084496131",
+    appId: "1:524084496131:web:f652a53ddfd5d018894b36",
+    measurementId: "G-KSNVHQP0JG"
+  };
